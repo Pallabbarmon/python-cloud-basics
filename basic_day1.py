c4ae9eaa86ebf6a9de20f163d6c3746016eccb4b
Pallabbarmon <pallabbarman02@gmail.com>
2025-12-17 21:31:30 +0600

    Day 1: Python basics

---
+++ basic_day1.py
@@ -0,0 +1,24 @@
+name = 'Pallab Barman'
+age = 24
+email = 'pallabbarman02@gmail.com'
+status = 'Student'
+
+print(f"Name : {name},\nAge : {age}, \nEmail : {email},\nStatus : {status}")
+
+uni_name = input("Enter your University name :")
+dept_name = input("Enter your department : ")
+
+print(f"{name},\n{status},\n{uni_name},\n{dept_name}")
+
+
+num_1 = float(input("Enter first number"))
+num_2 = float(input("Enter second number"))
+
+sum = num_1 + num_2
+sub = num_1 - num_2
+mul = num_1 * num_2
+div = num_1 / num_2
+mod = num_1 % num_2
+
+print(f"Addition: {sum}\nSubtraction: {sub}\nMultiplication: {mul}\nDivision: {div}\nModulus: {mod}")
+
