d87a78e43151101a506e10fe66fc3c6c267536f5
Pallabbarmon <pallabbarman02@gmail.com>
2025-12-17 22:13:01 +0600

    Day 2: lists, dicts and loops

--- basic_day2.py
+++ basic_day2.py
@@ -1,2 +1,9 @@
 my_list = ['notebook', 'pen', 'pc']
 print(f"{my_list}")
+
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
+city = input("Enter your city : ")
+
+my_dict = {"Name" : name, "Age" : age, "City": city}
+print(my_dict)
