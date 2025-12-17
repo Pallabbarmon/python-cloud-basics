1868300beacfd2e7f8b466382ad5d4660c501950
Pallabbarmon <pallabbarman02@gmail.com>
2025-12-17 22:25:24 +0600

    Day 2: lists, dicts and loops

--- basic_day2.py
+++ basic_day2.py
@@ -7,3 +7,22 @@ city = input("Enter your city : ")
 
 my_dict = {"Name" : name, "Age" : age, "City": city}
 print(my_dict)
+
+student = []
+
+while True :
+    name = input("Enter your name : (Or exit)")
+    if name.lower()== "exit":
+        break
+
+    marks = int(input("Enter your marks : "))
+    dict_student = {
+        "name" : name,
+        "marks" : marks
+    }
+    student.append(dict_student)
+
+    print("\nStudent List")
+
+    for s in student:
+        print(f"{s['name']} - {s['marks']} marks")
