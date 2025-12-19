3b6611675731cd6557e73b3e7adc5c4d4c51032f
Pallabbarmon <pallabbarman02@gmail.com>
2025-12-19 18:19:35 +0600

    Day 3: file handling

--- day3_file_handling.py
+++ day3_file_handling.py
@@ -1,21 +1,16 @@
 # File Handling
 
 file = open("students.txt", "a")
-
 name = input("Enter student name: ")
 marks = input("Enter marks: ")
-
 file.write(f"{name},{marks}\n")
 file.close()
-
 print("Data saved successfully!")
 
 # Read files
-file = open("students.txt", "a")
 
 file = open("students.txt", "r")
 
 content = file.read()
 print(content)
-
 file.close()
